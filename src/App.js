4ee2dedba8bbe16d7897017941d19747bb150587
Harvinder Singh <69596941+Hackersvilla@users.noreply.github.com>
2022-02-27 21:20:45 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
 import { CgMail } from "react-icons/cg";
-import Skill from './skill';
+import Skill from "./skill";
 import {
   Flex,
   Button,
@@ -13,48 +13,76 @@ import {
   useMediaQuery,
 } from "@chakra-ui/react";
 import Home from "./home";
-import {useWindowScrollPositions} from "./scrolldirection"
+import { useWindowScrollPositions } from "./scrolldirection";
 import About from "./About";
+import ClockLoader from "react-spinners/ClockLoader";
 
 export default function App() {
+  const [loading, setloading] = useState(false);
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
   const [isNotSmallerScreen] = useMediaQuery("(min-width:800px)");
 
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 5000);
+  }, []);
+
   return (
-    <>
-      <Flex p={30} direction="row" as="header" position="fixed" backgroundColor={isDark ? "transparent" : "white"} 
-  w="100%" h="10%" >
-        <IconButton
-        borderRadius= {10}
-          border="1px"
-          borderColor={isDark ? "white" : "black"}
-          variant="solid"
-          aria-label="Email_Button"
-          fontSize="30px"
-          icon={<CgMail />}
+    <div>
+      {loading ? (
+        <div style= {{
+          display:"flex",
+              alignItems: "center",
+              justifyContent:"center",
+              width:"100%",
+              height:"100vh"
+        }}>
+        <ClockLoader
+          color="#36D7B7"
+          loading={loading}
+          size={150}
         />
-       
-        <Spacer></Spacer>
-        <IconButton
-        borderRadius= {10}
-          border="1px"
-          borderColor={isDark ? "white" : "black"}
-          onClick={toggleColorMode}
-          icon={isDark ? <FaSun /> : <FaMoon />}
-        />
-      </Flex>
+        </div>
+      ) : (
+        <>
+          <Flex
+            p={30}
+            direction="row"
+            as="header"
+            position="fixed"
+            backgroundColor={isDark ? "transparent" : "white"}
+            w="100%"
+            h="10%"
+          >
+            <IconButton
+              borderRadius={10}
+              border="1px"
+              borderColor={isDark ? "white" : "black"}
+              variant="solid"
+              aria-label="Email_Button"
+              fontSize="30px"
+              icon={<CgMail />}
+            />
 
-      <Home 
-      isDark = {isDark}
-      isNotSmallerScreen = {isNotSmallerScreen}
-      ></Home>
-
-      <About
-      isDark = {isDark}
-      isNotSmallerScreen = {isNotSmallerScreen}
-      ></About>
-
-    </>
+            <Spacer></Spacer>
+            <IconButton
+              borderRadius={10}
+              border="1px"
+              borderColor={isDark ? "white" : "black"}
+              onClick={toggleColorMode}
+              icon={isDark ? <FaSun /> : <FaMoon />}
+            />
+          </Flex>
+          <Home isDark={isDark} isNotSmallerScreen={isNotSmallerScreen}></Home>
+          <About
+            isDark={isDark}
+            isNotSmallerScreen={isNotSmallerScreen}
+          ></About>
+        </>
+      )}
+    </div>
   );
 }
--- src/home.js
+++ src/home.js
@@ -1,28 +1,58 @@
 import React from "react";
 import {
-    Flex,
-    Button,
-    Spacer,
-    IconButton,
-    useColorMode,
-    Heading, 
-    useMediaQuery,
-  } from "@chakra-ui/react";
-
+  Flex,
+  Button,
+  Spacer,
+  IconButton,
+  useColorMode,
+  Heading,
+  Container,
+  Box,
+} from "@chakra-ui/react";
+import Typewriter from "typewriter-effect";
 
 export default function home(props) {
   return (
     <Flex direction="column" m={0} alignItems="center">
-      <Heading fontSize={props.isNotSmallerScreen ? "120px" : "70px"} mt="100" textAlign="center">
-        Software
+      <Heading
+        fontSize={props.isNotSmallerScreen ? "120px" : "70px"}
+        mt="100"
+        textAlign="center"
+      >
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("Software")
+              .pauseFor(5000)
+              .deleteAll()
+              .typeString("Graphic")
+              .pauseFor(5000)
+              .deleteAll()
+              .typeString("Self")
+              .start();
+          }}
+        />
       </Heading>
+
       <Heading
         fontSize={props.isNotSmallerScreen ? "120px" : "70px"}
         color="blue.400"
         mt="0"
         textAlign="center"
       >
-        Developer
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("Developer")
+              .pauseFor(5000)
+              .deleteAll()
+              .typeString("Designer")
+              .pauseFor(5000)
+              .deleteAll()
+              .typeString("Learner")
+              .start();
+          }}
+        />
       </Heading>
 
       <Button
@@ -30,7 +60,7 @@ export default function home(props) {
         mt={50}
         h={50}
         w={120}
-        boxShadow='dark-lg'
+        boxShadow="dark-lg"
         borderRadius={30}
         border="1px"
         borderColor={props.isDark ? "white" : "black"}
@@ -40,4 +70,4 @@ export default function home(props) {
       </Button>
     </Flex>
   );
-};
+}
