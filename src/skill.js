73439125d68f90a7b801e84c7a79f7d953392ced
Deepanshu Singh Negi <deepanshunegi10@gmail.com>
2022-03-06 21:14:07 +0530

    Update skill.js

--- src/skill.js
+++ src/skill.js
@@ -1,7 +1,7 @@
-import { Flex, Heading, Grid, GridItem, Text } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 import React from "react";
 
-export default function Skill(props) {
+export default function Skill() {
   return (  
     <>
       <Flex direction="column" mt={50}>
